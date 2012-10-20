20a6bcda69a3bae17b2b9d11de02745d42035fdd
Javier Domingo <javierdo1@gmail.com>
2012-10-20 20:18:06 +0200

    He acabado de poner la interfaz de administración facil para editar toda la base de datos, pero he cambiado el locale a inglés para que al usarla no estén las estructuras a usar en inglés y los objetos en castellano.

--- django_web/settings.py
+++ django_web/settings.py
@@ -30,7 +30,7 @@ TIME_ZONE = 'Europe/Madrid'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'es-es'
+LANGUAGE_CODE = 'en-en'
 
 SITE_ID = 1
 
--- members/admin.py
+++ members/admin.py
@@ -1,8 +1,41 @@
 from members.models import *
 from django.contrib import admin
 
+class PersonInstrumentRelationshipInline(admin.TabularInline):
+	model = PersonInstrumentRelationship
+	extra = 1
 
-admin.site.register(Person)
-admin.site.register(Instrument)
-admin.site.register(Post)
-admin.site.register(Email)
+class PersonPostRelationshipInline(admin.TabularInline):
+	model = PersonPostRelationship
+	extra = 1
+
+class PersonEmailRelationshipInline(admin.TabularInline):
+	model = PersonEmailRelationship
+	extra = 1
+
+class PersonAdmin(admin.ModelAdmin):
+	inlines = (
+		PersonInstrumentRelationshipInline,
+		PersonPostRelationshipInline,
+		PersonEmailRelationshipInline,
+	)
+
+class InstrumentAdmin(admin.ModelAdmin):
+	inlines = (
+		PersonInstrumentRelationshipInline,
+	)
+
+class PostAdmin(admin.ModelAdmin):
+	inlines = (
+		PersonPostRelationshipInline,
+	)
+
+class EmailAdmin(admin.ModelAdmin):
+	inlines = (
+		PersonEmailRelationshipInline,
+	)
+
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Instrument, InstrumentAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Email, EmailAdmin)
